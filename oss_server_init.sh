88066413bdf8b50d6d4e3d802569d5f0743ee4dd
Alex S <jalexspringer@gmail.com>
2017-10-06 19:50:59 -0400

    initial

---
+++ oss_server_init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# Redis Open Source Server Install
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+sudo cp src/redis-server /usr/local/bin/
+sudo cp src/redis-cli /usr/local/bin/
